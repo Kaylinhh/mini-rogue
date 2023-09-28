1a35c7538c69fb720999a53ba1563c5b3e45b6d8
Kaylinhh <ly.julie@live.fr>
2023-09-28 15:22:50 +0200

    generateEncounter in service done

--- src/app/shared/services/events.service.ts
+++ src/app/shared/services/events.service.ts
@@ -8,6 +8,8 @@ import { Monster } from 'src/app/models/monster.model';
 })
 export class eventService {
 
+    eventPercentageCut = [10,40,70,80];
+
     constructor(private http: HttpClient) { }
 
     _getMonsters$(): Observable<any> {
@@ -15,6 +17,23 @@ export class eventService {
         .pipe(
             map(json => json.data)
         )
+    }
+
+    generateEncounter(): string {
+        let encounter = Math.floor(Math.random() * 100);
+
+        if (encounter <= this.eventPercentageCut[0]) {
+            return "camp";
+        } else if (encounter >  this.eventPercentageCut[0] && encounter <= this.eventPercentageCut[1]) {
+            return "loot";
+        } else if (encounter >  this.eventPercentageCut[1] && encounter <= this.eventPercentageCut[2]) {
+            return "monster";
+        } else if (encounter >  this.eventPercentageCut[2] && encounter <= this.eventPercentageCut[3]) {
+            return "shop";
+        } else {
+            return "trap";
+        }
+
     }
 
 }

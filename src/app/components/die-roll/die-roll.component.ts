19fc318db2c33ab186619bf70b16994011f716c5
SternHillpocken <nicolas.guerin.pro@laposte.net>
2023-09-28 13:32:40 +0200

    Value for traps

--- src/app/components/die-roll/die-roll.component.ts
+++ src/app/components/die-roll/die-roll.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Player } from 'src/app/models/player.model';
 
 @Component({
   selector: 'app-die-roll',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./die-roll.component.scss']
 })
 export class DieRollComponent {
+
+  @Input()
+  player!: Player;
+
+  @Output()
+  blackDie: EventEmitter<number> = new EventEmitter;
+
+  @Output()
+  whiteDice: EventEmitter<number[]> = new EventEmitter;
+
+  ngOnInit(): void {
+    this.blackDie.emit(Math.floor(Math.random()*6)+1);
+    let whiteDiceGeneration: number[] = [];
+    for(let i = 0; i < this.player.die; i++){
+      whiteDiceGeneration.push(Math.floor(Math.random()*6)+1);
+    }
+    this.whiteDice.emit(whiteDiceGeneration);
+  }
+
+  roll(): void {
+    //
+  }
 
 }
--- src/app/models/trap.model.ts
+++ src/app/models/trap.model.ts
@@ -1,8 +1,10 @@
+import { TrapLine } from "./trap-line.model";
+
 export class Trap {
     constructor(
         public id: number,
         public name: string,
         public description: string,
-        public linesOfEffect: any[]
+        public linesOfEffect: TrapLine[]
     ){}
 }

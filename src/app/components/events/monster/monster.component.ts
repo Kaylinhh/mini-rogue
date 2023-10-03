fd5e0c572dcd26809eb473d7770169755067de07
Kaylinhh <ly.julie@live.fr>
2023-10-03 20:16:39 +0200

    fight() monsterComponent done

--- src/app/components/events/monster/monster.component.ts
+++ src/app/components/events/monster/monster.component.ts
@@ -15,15 +15,41 @@ export class MonsterComponent implements OnInit {
   @Input() player!: Player;
 
   monster!: Monster;
-  monsterList$!: Observable<Monster[]>;
+  monsterList!: Monster[];
+  playerDieRoll: number = 0;
+  monsterDieRoll: number = 0;
 
-
-  constructor(private monsterService: MonsterService) {
-  }
+  constructor(private monsterService: MonsterService,
+    private playerService: PlayerService,
+  ) { }
 
   ngOnInit(): void {
-    this.monsterList$ = this.monsterService.getMonstersByZone$(this.player.currentZone);
-    };
-  }
+    this.monsterService.getMonstersByZone$(this.player.currentZone).subscribe(data => {
+      this.monsterList = data;
+      [this.monster] = this.monsterList;
+    })
+  };
 
+  fight(): void {
+    
+    
+    while (this.player.life > 0 && this.monster.life > 0) { //so every iteration makes one round
+      this.playerDieRoll = Math.ceil(Math.random() * 6);
+      this.monsterDieRoll = Math.ceil(Math.random() * 6);
+      
+      //while player.life > 0, player attacks (first), then monster attacks => one round
+      //they need to succeed on their diceRoll to hit
+      
+      if(this.playerDieRoll >= 3) this.monster.life -= 2; //player's dmg to be discussed
+      if(this.monsterDieRoll === 6) this.player.life -= this.monster.damage; //ignores armor on crit
+      if(this.monsterDieRoll === 5) this.player.life -= this.player.armor - this.monster.damage;
+
+      console.log(this.player.life, this.playerDieRoll, this.monster.life, this.monsterDieRoll);
+      
+    }
+
+    this.playerService._setPlayer$(this.player);
+
+  }
+}
 
--- src/app/shared/services/monster.service.ts
+++ src/app/shared/services/monster.service.ts
@@ -5,51 +5,26 @@ import { Boss } from 'src/app/models/boss.model';
 import { Monster } from 'src/app/models/monster.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class MonsterService {
 
-    monster: Observable<Monster[]> = this.http.get<Monster[]>("/assets/json/monsters.json");
-    boss: Observable<Boss[]> = this.http.get<Boss[]>("/assets/json/bosses.json");
-    private _monstersByZoneSubject$: BehaviorSubject<Monster[]> = new BehaviorSubject<Monster[]>([]);
-    private _bossesByZoneSubject$: BehaviorSubject<Boss[]> = new BehaviorSubject<Boss[]>([]);
+  monster: Observable<Monster[]> = this.http.get<Monster[]>("/assets/json/monsters.json");
+  private _monstersByZoneSubject$: BehaviorSubject<Monster[]> = new BehaviorSubject<Monster[]>([]);
+  private _bossesByZoneSubject$: BehaviorSubject<Boss[]> = new BehaviorSubject<Boss[]>([]);
 
-    constructor(private http: HttpClient) { }
-
-    get monsterByZone$(): Observable<Monster[]> {
-        return this._monstersByZoneSubject$.asObservable();
-    }
-
-    get bossByZone$(): Observable<Monster[]> {
-        return this._monstersByZoneSubject$.asObservable();
-    }
-
-    // getMonstersByZone$(zone: number): Observable<Monster[]> {
-    //     this.monster
-    //       .subscribe(monster => {
-    //         this._monstersByZoneSubject$.next(monster.filter(monster => monster.zoneNumber === zone) as Monster[])
-    //       });
-    //     return this._monstersByZoneSubject$.asObservable();
-    // }
+  constructor(private http: HttpClient) { }
 
 
-    getMonstersByZone$(zone: number): Observable<Monster[]> {
-      return this.monster.pipe(
-        map((monsters: Monster[]) => monsters.filter(monster => monster.zoneNumber === zone)),
-        map((filteredMonsters: Monster[]) => {
-          const randomIndex = Math.floor(Math.random() * filteredMonsters.length);
-          console.log(randomIndex, filteredMonsters);
-                    
-          return [filteredMonsters[randomIndex]];
-        })
-      );
-    }
-    
-    // getBossByZone$(zone: number): Observable<Boss[]> {
-    //     this.boss
-    //       .subscribe(boss => {
-    //         this._bossesByZoneSubject$.next(boss.filter(boss => boss.zoneNumber === zone) as Boss[])
-    //       });
-    //     return this._bossesByZoneSubject$.asObservable();
-    // }
+  getMonstersByZone$(zone: number): Observable<Monster[]> {
+    return this.monster.pipe(
+      map((monsters: Monster[]) => monsters.filter(monster => monster.zoneNumber === zone)),
+      map((filteredMonsters: Monster[]) => {
+        const randomIndex = Math.floor(Math.random() * filteredMonsters.length);
+        console.log(randomIndex, filteredMonsters);
+        return [filteredMonsters[randomIndex]];
+      })
+    );
+  }
+
 }

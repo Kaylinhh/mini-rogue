cee55f00c48d481c8358eb9e4d1bd7822f6c5ceb
Nicolas Guérin <nicolas.guerin.pro@laposte.net>
2023-10-28 00:32:23 +0200

    Adding camp information for background purposes, and differents lines for differents zones

--- src/app/models/camp-choice.model.ts
+++ src/app/models/camp-choice.model.ts
@@ -3,7 +3,7 @@ import { QuantityType } from "./quantity-type.model";
 export class CampChoice {
     constructor(
         public name: string,
-        public description: string,
+        public description: string[],
         public reward: QuantityType
     ){}
 }
